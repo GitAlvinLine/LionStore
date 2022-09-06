a426d56aab10b183b769024c153934552b48b1ca
AE <aeiosdev@gmail.com>
2022-09-06 14:35:16 -0500

    Added the OnboardingScreen swiftUI view

---
+++ LionStore/UI/SwiftUI/OnboardingScreen.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingScreen.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/6/22.
+//
+
+import SwiftUI
+
+struct OnboardingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingScreen()
+    }
+}
