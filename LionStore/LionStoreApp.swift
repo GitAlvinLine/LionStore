a1ba62be3ed9e2f49b16fa037e32331a7d9d4ee9
AE <aeiosdev@gmail.com>
2022-09-06 11:35:08 -0500

    Initial Commit

---
+++ LionStore/LionStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LionStoreApp.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LionStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
