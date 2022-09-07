e858b850594b7e38fbb3ee338a1ff9ada4ea2d69
AE <aeiosdev@gmail.com>
2022-09-06 22:50:37 -0500

    Notify Handler with a selected option on selecting a Onboarding option

--- LionStoreTests/UI/SwiftUI/Onboarding/OnboardingStoreTests.swift
+++ LionStoreTests/UI/SwiftUI/Onboarding/OnboardingStoreTests.swift
@@ -7,10 +7,37 @@
 
 import XCTest
 
+enum OnboardingOption: String {
+    case signUp
+    case signIn
+}
+
+struct OnboardingStore {
+    let options: [OnboardingOption]
+    
+    private let handler: (OnboardingOption) -> Void
+    
+    internal init(options: [OnboardingOption], handler: @escaping (OnboardingOption) -> Void) {
+        self.options = options
+        self.handler = handler
+    }
+    
+    func select(_ option: OnboardingOption) {
+        self.handler(option)
+    }
+}
+
 class OnboardingStoreTests: XCTestCase {
     
-    func test() {
-        XCTFail("Initial failing test...")
+    func test_select_notifiesHandlerWithSelectedOption() {
+        var selectedOption: [OnboardingOption] = []
+        let sut = OnboardingStore(options: [.signUp, .signIn], handler: { option in
+            selectedOption.append(option)
+        })
+        
+        XCTAssertEqual(sut.options.count, 2)
+        sut.select(sut.options.randomElement()!)
+        XCTAssertEqual(selectedOption.count, 1)
     }
 
 }
