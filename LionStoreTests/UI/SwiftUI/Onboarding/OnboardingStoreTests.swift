9fe0d32c081ab4e808f031b20114d22182f2ff8c
AE <aeiosdev@gmail.com>
2022-09-06 20:07:36 -0500

    Created 'OnboardingStoreTests' file

---
+++ LionStoreTests/UI/SwiftUI/Onboarding/OnboardingStoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingStoreTests.swift
+//  LionStoreTests
+//
+//  Created by Alvin Escobar on 9/6/22.
+//
+
+import XCTest
+
+class OnboardingStoreTests: XCTestCase {
+    
+    func test() {
+        XCTFail("Initial failing test...")
+    }
+
+}
