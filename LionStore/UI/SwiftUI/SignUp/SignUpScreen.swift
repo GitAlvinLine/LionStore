2e9c08f8e748b66e147806437d1fe74f06fe5753
AE <aeiosdev@gmail.com>
2022-09-09 07:27:48 -0500

    Add Logo image and implemented logo image in UI in new SignUpScreen file

---
+++ LionStore/UI/SwiftUI/SignUp/SignUpScreen.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpScreen.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/8/22.
+//
+
+import SwiftUI
+
+struct SignUpScreen: View {
+    var body: some View {
+        Image("logo")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 100, height: 100)
+    }
+}
+
+struct SignUpScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpScreen()
+    }
+}
