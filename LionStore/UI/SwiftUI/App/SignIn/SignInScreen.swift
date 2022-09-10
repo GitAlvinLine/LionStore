7926c544e359ac9f0dae108864c94d114dc1a310
AE <aeiosdev@gmail.com>
2022-09-09 23:52:29 -0500

    Added SignInScreen file

---
+++ LionStore/UI/SwiftUI/App/SignIn/SignInScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SignInScreen.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/9/22.
+//
+
+import SwiftUI
+
+struct SignInScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInScreen()
+    }
+}
