bd1e325d73400c6ebcc199df826fdd0e9e98b042
AE <aeiosdev@gmail.com>
2022-09-09 23:58:29 -0500

    Add ResetPasswordScreen

---
+++ LionStore/UI/SwiftUI/App/ResetPasswordScreen/ResetPasswordScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ResetPasswordScreen.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/9/22.
+//
+
+import SwiftUI
+
+struct ResetPasswordScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResetPasswordScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ResetPasswordScreen()
+    }
+}
