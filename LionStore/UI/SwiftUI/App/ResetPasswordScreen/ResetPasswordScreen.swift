7299f68a331131a44cfb3e6ce4fbf3fc222d0fa3
AE <aeiosdev@gmail.com>
2022-09-09 23:59:16 -0500

    ResetPasswordScreen has text saying it's reset Password for testing

--- LionStore/UI/SwiftUI/App/ResetPasswordScreen/ResetPasswordScreen.swift
+++ LionStore/UI/SwiftUI/App/ResetPasswordScreen/ResetPasswordScreen.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ResetPasswordScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Reset password screen")
     }
 }
 
