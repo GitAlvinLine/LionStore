1b9226a513c491d01232f6c36b4980fc3114a771
AE <aeiosdev@gmail.com>
2022-09-10 21:56:23 -0500

    Implement the UI for 'Sign In' screen

--- LionStore/UI/SwiftUI/App/SignIn/SignInScreen.swift
+++ LionStore/UI/SwiftUI/App/SignIn/SignInScreen.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct SignInScreen: View {
+    @State private var email: String = ""
+    @State private var showPassword: Bool = false
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            AppLogo(image: .appLogo)
+            
+            Spacer()
+            
+            VStack(spacing: 20) {
+                EmailTextField(image: .email,
+                               placeholder: "Email",
+                               email: $email)
+                
+                PasswordTextField(image: .passwordLock,
+                                  showPassword: $showPassword,
+                                  password: $password)
+            }
+            
+            HStack {
+                Spacer()
+                AskAccountButton(text: "Forgot Password?",
+                                 textColor: .lightPurple) {
+                    print()
+                }
+            }
+            .padding(.trailing, 45)
+            .padding(.bottom, 40)
+            
+            CustomButton(text: "Sign In",
+                         textColor: .white,
+                         bg: .lightPurple) {
+                print()
+            }
+            
+            Spacer()
+            
+            AskAccountButton(text: "Create an account?",
+                             textColor: .lightPurple) {
+                print()
+            }
+
+        }
     }
 }
 
