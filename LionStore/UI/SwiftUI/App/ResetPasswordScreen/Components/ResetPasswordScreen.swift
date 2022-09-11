6e3352bb2f2b8bf3b69bb299270c7c6df764114e
AE <aeiosdev@gmail.com>
2022-09-10 22:32:03 -0500

    Extract 'ResetPasswordTextField' into it's own file:

--- LionStore/UI/SwiftUI/App/ResetPasswordScreen/Components/ResetPasswordScreen.swift
+++ LionStore/UI/SwiftUI/App/ResetPasswordScreen/Components/ResetPasswordScreen.swift
@@ -40,38 +40,6 @@ struct ResetPasswordScreen: View {
     }
 }
 
-struct ResetPasswordTextField: View {
-    let placeholder: String
-    let placeholderColor: AppColor
-    let textFieldColor: AppColor
-    @Binding var text: String
-    
-    var body: some View {
-        HStack {
-            ZStack {
-                HStack {
-                    Text(text.isEmpty ? placeholder : "")
-                        .foregroundColor(placeholderColor.value)
-                        .bold()
-                    Spacer()
-                }
-                TextField("",
-                          text: $text)
-            }
-            .padding(.leading, 15)
-        }
-        .padding(.leading, 45)
-        .padding(.trailing, 45)
-        .overlay(
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(textFieldColor.value, lineWidth: 2)
-                .padding(.leading, 45)
-                .padding(.trailing, 45)
-                .frame(width: ScreenSize.width, height: 50)
-        )
-    }
-}
-
 struct ResetPasswordScreen_Previews: PreviewProvider {
     static var previews: some View {
         ResetPasswordScreen()
