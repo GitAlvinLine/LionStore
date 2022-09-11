9f2f80601eceb07a179edf7908c696ec7c843781
AE <aeiosdev@gmail.com>
2022-09-11 15:37:39 -0500

    Create 'TabViewBar' view

---
+++ LionStore/UI/SwiftUI/App/TabView/TabViewBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabViewBar.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/11/22.
+//
+
+import SwiftUI
+
+struct TabViewBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabViewBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabViewBar()
+    }
+}
