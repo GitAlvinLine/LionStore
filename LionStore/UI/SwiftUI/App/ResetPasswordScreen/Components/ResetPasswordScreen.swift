2a24b51fee66ab7b35ff2fcdeb2b295c2621d28a
AE <aeiosdev@gmail.com>
2022-09-10 22:30:23 -0500

    Create 'ResetPasswordTextField' view and set padding on leading/trailing to 45

--- LionStore/UI/SwiftUI/App/ResetPasswordScreen/Components/ResetPasswordScreen.swift
+++ LionStore/UI/SwiftUI/App/ResetPasswordScreen/Components/ResetPasswordScreen.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ResetPasswordScreen: View {
+    @State private var text: String = ""
+    
     var body: some View {
         ZStack {
             VStack {
@@ -16,10 +18,57 @@ struct ResetPasswordScreen: View {
                 }
                 
                 RecoverAccountText()
+                
+                Spacer()
+                
+                ResetPasswordTextField(
+                    placeholder: "Enter email or phone number",
+                    placeholderColor: .textFieldBorder,
+                    textFieldColor: .textFieldBorder,
+                    text: $text)
+                
+                CustomButton(text: "Next",
+                             textColor: .white,
+                             bg: .lightPurple) {
+                    print()
+                }
+                             .padding(.top, 20)
 
                 Spacer()
             }
         }
+    }
+}
+
+struct ResetPasswordTextField: View {
+    let placeholder: String
+    let placeholderColor: AppColor
+    let textFieldColor: AppColor
+    @Binding var text: String
+    
+    var body: some View {
+        HStack {
+            ZStack {
+                HStack {
+                    Text(text.isEmpty ? placeholder : "")
+                        .foregroundColor(placeholderColor.value)
+                        .bold()
+                    Spacer()
+                }
+                TextField("",
+                          text: $text)
+            }
+            .padding(.leading, 15)
+        }
+        .padding(.leading, 45)
+        .padding(.trailing, 45)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(textFieldColor.value, lineWidth: 2)
+                .padding(.leading, 45)
+                .padding(.trailing, 45)
+                .frame(width: ScreenSize.width, height: 50)
+        )
     }
 }
 
