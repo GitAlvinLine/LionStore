13d7a28b8a28dfa20981d080110acf8e7bc1a72c
AE <aeiosdev@gmail.com>
2022-09-11 17:39:50 -0500

    Add NavigationLink for 'EditButton' to segue into Edit Profile Details

--- LionStore/UI/SwiftUI/App/ProfileTabScreen/ProfileTabScreen.swift
+++ LionStore/UI/SwiftUI/App/ProfileTabScreen/ProfileTabScreen.swift
@@ -11,25 +11,44 @@ struct ProfileTabScreen: View {
     let signOut: (OnboardingOption) -> Void
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image(systemName: "pencil")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(AppColor.lightPurple.value)
-                Spacer()
-                CustomButton(text: "Log Out", textColor: .white, bg: .lightPurple) {
-                    signOut(.signIn)
+        NavigationView {
+            ZStack {
+                VStack {
+                    HStack {
+                        Spacer()
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            Image("edit")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                                .padding()
+                                .foregroundColor(AppColor.lightPurple.value)
+                        }
+                    }
+                    .padding(.trailing, 30)
+
+
+                    Spacer()
+                    CustomButton(text: "Log Out", textColor: .white, bg: .lightPurple) {
+                        signOut(.signIn)
+                    }
+                    .padding(.bottom, 10)
                 }
-                .padding(.bottom, 10)
             }
+            .navigationBarHidden(true)
         }
     }
 }
 
 struct ProfileTabScreen_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileTabScreen(signOut: { _ in})
+        NavigationView {
+            ZStack {
+                ProfileTabScreen(signOut: { _ in})
+            }
+            .navigationBarHidden(true)
+        }
     }
 }
