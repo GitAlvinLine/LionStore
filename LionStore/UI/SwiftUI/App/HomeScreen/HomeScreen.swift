80691804a7744bd0a8a382268eb08fc94db0a4ce
AE <aeiosdev@gmail.com>
2022-09-12 21:21:47 -0500

    Add HomeScreen file

---
+++ LionStore/UI/SwiftUI/App/HomeScreen/HomeScreen.swift
@@ -0,0 +1,27 @@
+//
+//  HomeScreen.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/12/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                VStack {
+                    
+                }
+            }
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
