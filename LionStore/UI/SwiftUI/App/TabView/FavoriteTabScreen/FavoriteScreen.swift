a0f3c763f2adf9fddfe436919dc902a1532fa2dd
AE <aeiosdev@gmail.com>
2022-09-20 10:49:49 -0500

    Create 'FavoriteScreen' file

---
+++ LionStore/UI/SwiftUI/App/TabView/FavoriteTabScreen/FavoriteScreen.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteScreen.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/20/22.
+//
+
+import SwiftUI
+
+struct FavoriteScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteScreen()
+    }
+}
