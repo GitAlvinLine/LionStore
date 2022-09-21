6e590bf173473c64c12156b9b01ccccb8a6f6cc2
AE <aeiosdev@gmail.com>
2022-09-21 17:05:30 -0500

    Create 'MenuButton' view for sorting items

--- LionStore/UI/SwiftUI/App/TabView/FavoriteTabScreen/FavoriteScreen.swift
+++ LionStore/UI/SwiftUI/App/TabView/FavoriteTabScreen/FavoriteScreen.swift
@@ -7,55 +7,95 @@
 
 import SwiftUI
 
+enum SortType {
+    case price
+    case date
+}
+
+struct SortOption: Identifiable {
+    var id = UUID()
+    var type: SortType
+    var text: String
+}
+
 struct FavoriteScreen: View {
+    
+    @State private var sortType: SortType = .price
+    
     var body: some View {
         ZStack {
             VStack {
                 HStack {
                     Spacer()
-                    Menu {
-                        Button {
-                            print("sort by price")
-                        } label: {
-                            Text("Sort by price")
-                        }
-                        
-                        Button {
-                            print("sort by date")
-                        } label: {
-                            Text("Sort by date")
-                        }
-                        
-                        if #available(iOS 15.0, *) {
-                            Button(role: .destructive) {
-                                print("remove all items")
-                            } label: {
-                                Text("Remove all items")
-                            }
-                        } else {
-                            // Fallback on earlier versions
-                            Button {
-                                print("remove all items")
-                            } label: {
-                                Text("Remove all items")
-                            }
-                        }
-
-
-                    } label: {
-                        Image("optionsIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30)
-                            .padding(10)
-                    }
-                    .foregroundColor(AppColor.lightPurple.value)
+                    MenuButton(sortType: $sortType)
                     
                 }
                 .padding(.trailing, 25)
                 Spacer()
             }
             .padding(.top, 60)
+        }
+    }
+}
+
+struct MenuButton: View {
+    @Binding var sortType: SortType
+    
+    private let sortOptions: [SortOption] = [
+        SortOption(type: .price,
+                   text: "Sort by Price"),
+        SortOption(type: .date,
+                   text: "Sort by date")
+    ]
+    
+    var body: some View {
+        Menu {
+            ForEach(sortOptions) { option in
+                MenuSortOptionButton(
+                    sortOption: option,
+                    selectedSortType: $sortType) { sortOption in
+                        
+                    }
+            }
+            
+            if #available(iOS 15.0, *) {
+                Button(role: .destructive) {
+                    print("remove all items")
+                } label: {
+                    Text("Remove all items")
+                }
+            } else {
+                // Fallback on earlier versions
+                Button {
+                    print("remove all items")
+                } label: {
+                    Text("Remove all items")
+                }
+            }
+
+
+        } label: {
+            Image("optionsIcon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .padding(10)
+        }
+        .foregroundColor(AppColor.lightPurple.value)
+    }
+}
+
+struct MenuSortOptionButton: View {
+    let sortOption: SortOption
+    @Binding var selectedSortType: SortType
+    let action: (SortType) -> Void
+    
+    var body: some View {
+        Button {
+            selectedSortType = sortOption.type
+            action(sortOption.type)
+        } label: {
+            Label(sortOption.text, systemImage: sortOption.type == $selectedSortType.wrappedValue ? "checkmark" : "")
         }
     }
 }
