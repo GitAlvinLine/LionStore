173dbc4acffd4006ffae45b5a1b00212b66fa5eb
AE <aeiosdev@gmail.com>
2022-09-22 14:06:53 -0500

    Add 'CartScreen' file

---
+++ LionStore/UI/SwiftUI/App/TabView/CartScreen/CartScreen.swift
@@ -0,0 +1,20 @@
+//
+//  CartScreen.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/22/22.
+//
+
+import SwiftUI
+
+struct CartScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CartScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        CartScreen()
+    }
+}
