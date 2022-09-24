b1f50fea1fdd056c125de369506008c3cfdf013e
AE <aeiosdev@gmail.com>
2022-09-24 18:18:53 -0500

    Refactor tests to remove duplication

--- LionStoreTests/FirebaseAuthLoaderTests.swift
+++ LionStoreTests/FirebaseAuthLoaderTests.swift
@@ -30,34 +30,39 @@ protocol FirebaseAuthClient {
     func signIn(with credentials: LoginCredentials)
 }
 
-class FirebaseAuthClientSpy: FirebaseAuthClient {
-    var userCredentials: LoginCredentials?
-    
-    func signIn(with credentials: LoginCredentials) {
-        userCredentials = credentials
-    }
-}
-
 class FirebaseAuthLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestSignInFromFirebaseAuth() {
-        let client = FirebaseAuthClientSpy()
-        let credentials = LoginCredentials(email: "", password: "")
-        let _ = FirebaseAuthLoader(credentials: credentials, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.userCredentials?.email)
         XCTAssertNil(client.userCredentials?.password)
     }
     
     func test_signIn_requestsSignInFromFirebaseAuth() {
-        let client = FirebaseAuthClientSpy()
         let credentials = LoginCredentials(email: "", password: "")
-        let sut = FirebaseAuthLoader(credentials: credentials, client: client)
+        let (sut, client) = makeSUT(credentials: credentials)
         
         sut.signIn()
         
         XCTAssertNotNil(client.userCredentials?.email)
         XCTAssertNotNil(client.userCredentials?.password)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(credentials: LoginCredentials = LoginCredentials(email: "", password: "")) -> (sut: FirebaseAuthLoader, client: FirebaseAuthClientSpy) {
+        let client = FirebaseAuthClientSpy()
+        let sut = FirebaseAuthLoader(credentials: credentials, client: client)
+        return (sut, client)
+    }
+    
+    private class FirebaseAuthClientSpy: FirebaseAuthClient {
+        var userCredentials: LoginCredentials?
+        
+        func signIn(with credentials: LoginCredentials) {
+            userCredentials = credentials
+        }
     }
     
 }
