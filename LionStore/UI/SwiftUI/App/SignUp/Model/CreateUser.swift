16d8e437960060bafc3049612a02016b49bcd1c3
AE <aeiosdev@gmail.com>
2022-09-27 16:50:45 -0500

    Create 'CreateUser' model for SignUpScreen

---
+++ LionStore/UI/SwiftUI/App/SignUp/Model/CreateUser.swift
@@ -0,0 +1,15 @@
+//
+//  CreateUser.swift
+//  LionStore
+//
+//  Created by Alvin Escobar on 9/27/22.
+//
+
+import Foundation
+
+struct CreateUser {
+    var email: String
+    var phoneNumber: String
+    var password: String
+    var verifyPassword: String
+}
