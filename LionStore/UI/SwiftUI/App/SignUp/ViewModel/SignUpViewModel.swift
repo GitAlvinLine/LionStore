c59a8adf52cb35aaa938dd0467c42697abbe2033
AE <aeiosdev@gmail.com>
2022-09-27 17:13:09 -0500

    declare createUser method to call FirebaseAuth 'createUser' method to signUp

--- LionStore/UI/SwiftUI/App/SignUp/ViewModel/SignUpViewModel.swift
+++ LionStore/UI/SwiftUI/App/SignUp/ViewModel/SignUpViewModel.swift
@@ -13,8 +13,28 @@ final class SignUpViewModel: ObservableObject {
     @Published var showPassword: Bool = false
     
     private(set) var firebaseAuth: AuthService
+    private var cancellables = Set<AnyCancellable>()
     
     init(_ authService: AuthService) {
         self.firebaseAuth = authService
+    }
+}
+
+// MARK: - Sign Up User
+extension SignUpViewModel {
+    func createUser() {
+        firebaseAuth
+            .createUser(with: user.email, and: user.password)
+            .sink { result in
+                switch result {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error)
+                }
+            } receiveValue: { _ in
+                
+            }
+            .store(in: &cancellables)
     }
 }
